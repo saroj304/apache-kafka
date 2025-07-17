c17d1b078fefc02dc3cc2f99f2a28dce86e15a59
sarojkhatiwada <sarojkhatiwada1999@gmail.com>
2025-07-17 14:58:44 +0545

    created topic

---
+++ src/main/java/com/kafka_practice_app/productmicroservice/ProductmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.kafka_practice_app.productmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductmicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka_practice_app/productmicroservice/ProductmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka_practice_app.productmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
